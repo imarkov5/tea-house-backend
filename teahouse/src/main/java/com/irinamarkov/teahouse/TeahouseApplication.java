4b0e15d299f2c358fda37b87b462aa67dc3a02a5
Irina Markov <irina@aquilatech.com>
2021-04-17 12:04:25 -0500

    created teahouse spring boot project and connected to mysql

---
+++ teahouse/src/main/java/com/irinamarkov/teahouse/TeahouseApplication.java
@@ -0,0 +1,13 @@
+package com.irinamarkov.teahouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeahouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeahouseApplication.class, args);
+	}
+
+}
